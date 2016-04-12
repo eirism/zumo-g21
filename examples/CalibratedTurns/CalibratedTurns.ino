2381abdad50a20e28623910cabc7a9f16e3b715f
Eirik <eirik.1996@hotmail.com>
2016-04-12 15:15:33 +0200

    Update CalibratedTurns.ino
    Removes unnecessary comments

--- examples/CalibratedTurns/CalibratedTurns.ino
+++ examples/CalibratedTurns/CalibratedTurns.ino
@@ -5,10 +5,7 @@
 #include <L3G.h>
 #include <LSM303.h>
 
-// added a symbolic link from zumo-g21\libraries to my arduino\libraries folder. this works.
 #include <CalibratedTurns.h>
-//#include "../../libraries/CalibratedTurns/CalibratedTurns.h"
-//#include "C:/Users/admin/Dropbox/code/ProgLab/zumo-g21/libraries/CalibratedTurns/CalibratedTurns.hpp"
 
 void setup(){
 	CalibratedTurnsSetup();
